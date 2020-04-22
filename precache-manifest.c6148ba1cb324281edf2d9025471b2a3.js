16d0469e8464fab82829e21e74058d29bf16966f
Ahmad-Sawalqeh <53290785+Ahmad-Sawalqeh@users.noreply.github.com>
2020-04-22 19:28:25 +0300

    Updates

---
+++ precache-manifest.c6148ba1cb324281edf2d9025471b2a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ca988575c080b622c3f318c37be9823",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6800d784a50811fc5548",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "6dee8032799991bbee47",
+    "url": "/static/css/main.4d8b2dff.chunk.css"
+  },
+  {
+    "revision": "6800d784a50811fc5548",
+    "url": "/static/js/2.5829547e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5829547e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dee8032799991bbee47",
+    "url": "/static/js/main.08850aa4.chunk.js"
+  },
+  {
+    "revision": "016a6ae85f7c9c82f278",
+    "url": "/static/js/runtime-main.2900b212.js"
+  }
+]);
